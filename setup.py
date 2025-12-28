795015930413bee398d3b4f35d9f342e9d50e60d
Spiros Evangelatos <sevangelatos@gmail.com>
2025-12-28 14:52:56 +0200

    Improve compilation flags

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ import os
 from io import open
 import setuptools
 
+VERSION_STRING='0.2.0'
+
 EXT_SOURCES = [
     "JUCE/modules/juce_core/juce_core.cpp",
     "JUCE/modules/juce_cryptography/juce_cryptography.cpp",
@@ -13,12 +15,16 @@ INT_SOURCES = [
     "src/juce_rsa.cpp"
 ]
 
+# Platform-specific configuration
 if sys.platform == "win32":
+    # Windows
     cflags = [
-        "/std:c++latest",
+        "/std:c++17",
         "/O2",
+        "/DNDEBUG",
         "/DJUCE_GLOBAL_MODULE_SETTINGS_INCLUDED",
-        "/DJUCE_STANDALONE_APPLICATION"
+        "/DJUCE_STANDALONE_APPLICATION",
+        "/DJUCE_USE_CURL=0"
     ]
     libs = [
         "Shell32",
@@ -26,10 +32,15 @@ if sys.platform == "win32":
     ]
     lflags = []
 else:
+    # Linux and other Unix-like systems
     cflags = [
+        "-std=c++17",
+        "-O3",
+        "-DNDEBUG",
         "-DJUCE_GLOBAL_MODULE_SETTINGS_INCLUDED",
         "-DJUCE_STANDALONE_APPLICATION",
         "-DJUCE_USE_CURL=0",
+        "-fPIC"  # Position Independent Code for shared libraries
     ]
     libs = [
     ]
@@ -37,8 +48,9 @@ else:
 
 # Obtain JUCE if it is not already here
 if not os.path.isfile(EXT_SOURCES[0]):
-    os.system("git clone https://github.com/juce-framework/JUCE.git")
-    os.system("cd JUCE; git checkout 5.4.7")
+    print("JUCE not found, downloading...")
+    os.system("git clone --depth 1 --branch 8.0.12 https://github.com/juce-framework/JUCE.git")
+    print("JUCE downloaded successfully")
 
 extensions = [setuptools.Extension(
     "juce_rsa",
@@ -47,7 +59,8 @@ extensions = [setuptools.Extension(
     extra_link_args=lflags,
     include_dirs=["JUCE/modules"],
     libraries=libs,
-    language="c++"
+    language="c++",
+    define_macros=[('VERSION_INFO', VERSION_STRING)]  # Version info for the extension
 )]
 
 
@@ -58,7 +71,7 @@ setuptools.setup(
     author="sevangelatos",
     author_email="sevangelatos_at_@gmail.com",
     name="juce_rsa",
-    version="0.1",
+    version=VERSION_STRING,
     url="https://github.com/sevangelatos/juce_rsa",
     ext_modules=extensions,
     description="Python bindings for juce::RSAKey",
